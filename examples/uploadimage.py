a50a6ebf082dc6b17bf841577cdde899b82c2f8c
Christian Stocker <me@chregu.tv>
2018-05-03 11:45:41 +0200

    add usermeta example

--- examples/uploadimage.py
+++ examples/uploadimage.py
@@ -17,8 +17,10 @@ api_client = rokka_client_codegen.ApiClient(config)
 api_instance = rokka_client_codegen.SourceimagesApi(api_client)
 
 try:
-    api_response = api_instance.create_source_image(filedata="image.png", organization=organization)
+    api_response = api_instance.create_source_image(filedata="image.png", organization=organization, meta_user= '{"foo": "bar"}')
     pprint(api_response.items)
     print("\nHash is " + api_response.items[0].hash)
+    print("\nUsermeta is " + str(api_response.items[0].user_metadata))
+    print("\nUsermeta foo is " + api_response.items[0].user_metadata['foo']);
 except ApiException as e:
     print("Exception when calling StacksApi->list_stacks: %s\n" % e)
